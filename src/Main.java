3673f5e1a6d01477a223da972d612e55d563badf
Max Paulo <maxpauloteixeira@gmail.com>
2024-03-05 15:35:50 -0300

    testing add the graph in the new version

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 package src;
 
-import src.com.max.Dijkstra;
+import src.com.max.*;
 
 public class Main{
 
     public static void main(String[] args){
-        Dijkstra dijkstra = new Dijkstra();
 
-        dijkstra.runDijkstra();
+        Graph grafo = new Graph(4);
+
+        Children childInicio = new Children(new String[]{"A", "B"}, new double[]{6.0, 2.0});
+        Children childA = new Children(new String[]{"fim"}, new double[]{1.0});
+        Children childB = new Children(new String[]{"fim", "A"}, new double[]{5.0, 3.0});
+        Children childFim = null;
+
+        grafo.addNode("inicio", childInicio);
+        grafo.addNode("A", childA);
+        grafo.addNode("B", childB);
+        grafo.addNode("fim", childFim);
+        
+        grafo.graphVisualiser();
     }
     
 }
